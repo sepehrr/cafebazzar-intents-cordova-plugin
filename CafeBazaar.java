47eda0d788c341c691284f55dd37d601afd6c93f
sepehrr <ssriran@gmail.com>
2016-07-06 04:11:31 +0430

    Add files via upload

---
+++ CafeBazaar.java
@@ -0,0 +1,128 @@
+/**
+ * The MIT License
+ *
+ *	Copyright (c) 2016
+ *	Sepehr Raftari
+ *	
+ *	
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+package com.epsi.plugins.cordova;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import android.util.Log;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Style and such borrowed from the TTS and PhoneListener plugins
+ */
+public class CafeBazaar extends CordovaPlugin {
+    private static final String LOG_TAG = CafeBazaar.class.getSimpleName();
+    private static int REQUEST_CODE = 1001;
+
+    // private CallbackContext callbackContext;
+    // private LanguageDetailsChecker languageDetailsChecker;
+
+    //@Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+		// Boolean isValidAction = true;
+
+  //   	this.callbackContext= callbackContext;
+
+		// // Action selector
+  //   	if ("startRecognize".equals(action)) {
+  //           // recognize speech
+  //           startSpeechRecognitionActivity(args);     
+  //       } else if ("getSupportedLanguages".equals(action)) {
+  //       	getSupportedLanguages();
+  //       } else if ("checkSpeechRecognition".equals(action)) {
+  //       	checkSpeechRecognition();
+  //       } else {
+  //           // Invalid action
+  //       	this.callbackContext.error("Unknown action: " + action);
+  //       	isValidAction = false;
+  //       }
+
+        if (action.equals("display-app")) {
+            try {
+                JSONObject arg_object = args.getJSONObject(0);
+                String app = arg_object.getString("app");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("bazaar://details?id=" + app));
+                intent.setPackage("com.farsitel.bazaar");
+                this.cordova.getActivity().startActivity(intent);
+                callbackContext.success("success");
+                return true;
+            } catch(Exception e) {
+                System.err.println("Exception: " + e.getMessage());
+                callbackContext.error(e.getMessage());
+                return false;
+            } 
+        }
+    	
+       if (action.equals("rate-app")) {
+            try {
+                JSONObject arg_object = args.getJSONObject(0);
+                String app = arg_object.getString("app");
+                Intent intent = new Intent(Intent.ACTION_EDIT); 
+                intent.setData(Uri.parse("bazaar://details?id=" + app));
+                intent.setPackage("com.farsitel.bazaar");
+                this.cordova.getActivity().startActivity(intent);
+                callbackContext.success("success");
+                return true;
+            } catch(Exception e) {
+                System.err.println("Exception: " + e.getMessage());
+                callbackContext.error(e.getMessage());
+                return false;
+            } 
+        }
+
+        if (action.equals("disp-developer")) {
+            try {
+                JSONObject arg_object = args.getJSONObject(0);
+                String developer = arg_object.getString("developer");
+                Intent intent = new Intent(Intent.ACTION_VIEW); 
+                intent.setData(Uri.parse("bazaar://collection?slug=by_author&aid=" + developer)); 
+                intent.setPackage("com.farsitel.bazaar"); 
+                this.cordova.getActivity().startActivity(intent);
+                callbackContext.success("success");
+                return true;
+            } catch(Exception e) {
+                System.err.println("Exception: " + e.getMessage());
+                callbackContext.error(e.getMessage());
+                return false;
+            } 
+        }
+
+        //callbackContext.error("false command");
+        return true;
+
+
+    }
+
+
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# cafebazzar-intents-cordova-plugin
-CafeBazaar Intents Plugin for Cordova/Phonegap; Rate and View apps on CafeBazaar
+CafeBazaar Intents Plugin for Cordova/Phonegap
+===================================
+
+Requirements
+-------------
+Android 2.2 (API level 8) is required  
+Compatible with Cordova 3.0 and above.
+
+Usage
+-------
+CafeBazaar is biggest android applications marketplace in Iran. This plugin let you use CafeBazaar intents on your Cordova/Phonegap App.
+Use this plugin only if you plan to release your Application on CafeBazaar.
+
+if you want to show the page of an application on CafeBazaar:
+<script type="text/javascript">
+    CafeBazaar.viewApp(successHandler, errorHandler, appPackageName);
+</script>
+
+if you want to let user to rate an application and put comment for it on CafeBazaar:
+<script type="text/javascript">
+    CafeBazaar.rateApp(successHandler, errorHandler, appPackageName);
+</script>
+
+if you want to show all your applications on CafeBazaar:
+<script type="text/javascript">
+    CafeBazaar.viewDeveloperPage(successHandler, errorHandler, yourCafeBazaarDeveloperId);
+</script>
+
+
+Full example
+----------------
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CafeBazaar Intents for Cordova/Phonegap</title>
+        <script type="text/javascript" src="cordova.js"></script>
+    </head>
+    <body>
+        <button type="button" onclick="CafeBazaar.rateApp(null, null, 'com.google.android.apps.maps');">Rate and comment for Google Maps App</button><br>
+        <button type="button" onclick="CafeBazaar.viewApp(null, null, 'com.google.android.apps.maps');">Show Google Maps App page on bBazaar</button><br>
+        <button type="button" onclick="CafeBazaar.viewDeveloperPage(null, null, 'sepehrr');">My apps on Bazaar</button><br>
+    </body>
+</html>
+```
+
+License
+----------------
+
+The MIT License
+
+Copyright (c) 2016
+Sepehr Raftari
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
